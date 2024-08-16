3642512b850f392687dc7761a4a9d05580d68609
o98k-ok <hggend@gmail.com>
2024-08-16 16:39:43 +0800

    [feature]<file>: file version merge ✨✨

--- README.md
+++ README.md
@@ -19,3 +19,23 @@ Usage of ./bin/center/merge-img:
   -out string
     	Output Image path
 ```
+
+
+### Image outline
+
+```shell
+./bin/template/file/img-outline -front ./testdata/test_jpg.jpg -out ./testdata/file_version.jpg
+./bin/template/file/img-outline -front ./testdata/test_jpg.jpg -out ./testdata/file_version.jpg -style raycast
+```
+
+![alt text](./testdata/file_version.jpg)
+
+```shell
+Usage of ./bin/template/file/img-outline:
+  -front string
+        Front image path
+  -out string
+        Output Image path
+  -style string
+        Merge style: macos/raycast (default "macos")
+```
---
+++ bin/template/file/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"os"
+	"path"
+	"time"
+
+	"math/rand"
+
+	"github.com/o98k-ok/img-outline/format"
+	"github.com/o98k-ok/img-outline/merge"
+)
+
+func main() {
+	var front string
+	var out string
+	var style string
+	var err error
+	flag.StringVar(&front, "front", "", "Front image path")
+	flag.StringVar(&out, "out", "", "Output Image path")
+	flag.StringVar(&style, "style", "macos", "Merge style: macos/raycast")
+	flag.Parse()
+
+	if front == "" || out == "" {
+		flag.Usage()
+		return
+	}
+	var back string
+	{
+		dir := path.Join("./bin/template/imgs", style)
+		files, err := os.ReadDir(dir)
+		if err != nil {
+			fmt.Println("Error reading directory:", dir, err)
+			return
+		}
+		if len(files) == 0 {
+			fmt.Println("No files found in the directory.", dir)
+			return
+		}
+		rand.Seed(time.Now().UnixNano())
+		randomIndex := rand.Intn(len(files))
+		back = path.Join(dir, files[randomIndex].Name())
+	}
+
+	var frontdata, backdata []byte
+	var fw, fh, bw, bh int
+	jpgHandler := format.NewJPGImage()
+	{
+		frontdata, err = os.ReadFile(front)
+		if err != nil {
+			fmt.Println(front, err)
+			flag.Usage()
+			return
+		}
+
+		var d bytes.Buffer
+		if err = format.ToJPG(bytes.NewReader(frontdata), &d); err != nil {
+			fmt.Println(front, err)
+			flag.Usage()
+			return
+		}
+		frontdata = d.Bytes()
+		fw, fh = jpgHandler.ImageSize(bytes.NewReader(frontdata))
+	}
+
+	{
+		backdata, err = os.ReadFile(back)
+		if err != nil {
+			fmt.Println(front, err)
+			flag.Usage()
+			return
+		}
+
+		var d bytes.Buffer
+		if err = format.ToJPG(bytes.NewReader(backdata), &d); err != nil {
+			fmt.Println(back, err)
+			flag.Usage()
+			return
+		}
+		backdata = d.Bytes()
+		bw, bh = jpgHandler.ImageSize(bytes.NewReader(backdata))
+	}
+
+	file, err := os.Create(out)
+	if err != nil {
+		fmt.Println(out, err)
+		flag.Usage()
+		return
+	}
+	defer file.Close()
+
+	x, y := merge.CenterCoordinate(fw, fh, bw, bh)
+	if err = merge.AppendOutline(frontdata, backdata, x, y, file); err != nil {
+		fmt.Println(out, err)
+		flag.Usage()
+		return
+	}
+}
